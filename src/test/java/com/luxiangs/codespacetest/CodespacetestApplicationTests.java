ac425bc1e32dde3c04b9c8e9825959fbd1e088e4
luxiang <luxiang19960412@gmail.com>
2022-06-12 16:57:30 +0800

    init

---
+++ src/test/java/com/luxiangs/codespacetest/CodespacetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxiangs.codespacetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespacetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
